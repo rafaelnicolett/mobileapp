858b90d62723e8b466d5bdeb1d2b88b195be8130
Artūrs Eimanis <arturseimanis@gmail.com>
2017-06-12 14:23:00 +0300

    :construction: Add ProjectsApi

--- Toggl.Ultrawave/ApiClients/Interfaces/IProjectsApi.cs
+++ Toggl.Ultrawave/ApiClients/Interfaces/IProjectsApi.cs
@@ -1,6 +1,11 @@
-﻿namespace Toggl.Ultrawave.ApiClients
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toggl.Ultrawave.ApiClients
 {
     public interface IProjectsApi
     {
+        IObservable<List<Project>> GetAll();
+        IObservable<Project> Create(Project timeEntry);
     }
 }
--- Toggl.Ultrawave/ApiClients/ProjectsApi.cs
+++ Toggl.Ultrawave/ApiClients/ProjectsApi.cs
@@ -1,6 +1,27 @@
-﻿namespace Toggl.Ultrawave.ApiClients
+﻿using System;
+using System.Collections.Generic;
+using Toggl.Ultrawave.Network;
+using Toggl.Ultrawave.Serialization;
+
+namespace Toggl.Ultrawave.ApiClients
 {
-    internal sealed class ProjectsApi : IProjectsApi
+    internal sealed class ProjectsApi : BaseApi, IProjectsApi
     {
+        private readonly ProjectEndpoints endPoints;
+
+        public ProjectsApi(ProjectEndpoints endPoints, IApiClient apiClient, IJsonSerializer serializer, Credentials credentials)
+            : base(apiClient, serializer, credentials)
+        {
+            this.endPoints = endPoints;
+        }
+
+        public IObservable<Project> Create(Project project)
+        {
+            var endPoint = endPoints.Post(project.WorkspaceId);
+            return CreateObservable(endPoint, AuthHeader, project, SerializationReason.Post);
+        }
+
+        public IObservable<List<Project>> GetAll()
+            => CreateObservable<List<Project>>(endPoints.Get, AuthHeader);
     }
 }

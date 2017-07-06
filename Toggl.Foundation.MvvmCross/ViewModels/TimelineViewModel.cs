7c368e97b150ee3d6278603dd8ba286dc97f631b
Artūrs Eimanis <arturseimanis@gmail.com>
2017-07-06 10:14:05 +0300

    :Construction: Add TimelineViewModel

---
+++ Toggl.Foundation.MvvmCross/ViewModels/TimelineViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+using Toggl.Foundation.DataSources;
+using Toggl.Multivac;
+using Toggl.Multivac.Models;
+
+namespace Toggl.Foundation.MvvmCross.ViewModels
+{
+    public class TimelineViewModel : MvxViewModel
+    {
+        private readonly ITogglDataSource dataSource;
+
+        public ObservableCollection<ITimeEntry> TimeEntries { get; }
+            = new ObservableCollection<ITimeEntry>();
+
+        public TimelineViewModel(ITogglDataSource dataSource)
+        {
+            Ensure.Argument.IsNotNull(dataSource, nameof(dataSource));
+
+            this.dataSource = dataSource;
+        }
+
+        public async override Task Initialize()
+        {
+            await base.Initialize();
+
+            TimeEntries.AddRange(await dataSource.TimeEntries.GetAll());
+        }
+    }
+}

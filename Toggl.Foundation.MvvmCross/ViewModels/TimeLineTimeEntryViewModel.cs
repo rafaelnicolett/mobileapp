3bb1e93de4b738d072c7229121596c0ad75e4990
Arturs Eimanis <arturseimanis@gmail.com>
2017-08-23 16:20:11 +0300

    :construction: Add TimeLineTimeEntryViewModel

---
+++ Toggl.Foundation.MvvmCross/ViewModels/TimeLineTimeEntryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Toggl.Multivac;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.Foundation.MvvmCross.ViewModels
+{
+    public class TimeLineTimeEntryViewModel
+    {
+        private const string defaultColor = "#CECECE";
+
+        public DateTimeOffset Start { get; }
+
+        public DateTimeOffset Stop { get; }
+
+        public string ProjectColor { get; }
+
+        public TimeLineTimeEntryViewModel(IDatabaseTimeEntry timeEntry)
+        {
+            Ensure.Argument.IsNotNull(timeEntry, nameof(timeEntry));
+
+            if (timeEntry.Stop == null)
+                throw new ArgumentException("Can't use running time entry (Stop must be set).");
+
+            Start = timeEntry.Start;
+            Stop = timeEntry.Stop.Value;
+            ProjectColor = timeEntry?.Project?.Color ?? defaultColor;
+        }
+    }
+}

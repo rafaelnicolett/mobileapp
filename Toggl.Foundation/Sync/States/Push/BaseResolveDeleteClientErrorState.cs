6adcf4dba92098851a95fc1b7add45fe84c98301
Simon Rozsival <simon@rozsival.com>
2017-10-19 13:37:31 +0200

    :sparkles: Add state

---
+++ Toggl.Foundation/Sync/States/Push/BaseResolveDeleteClientErrorState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Linq;
+using Toggl.Ultrawave.Exceptions;
+
+namespace Toggl.Foundation.Sync.States
+{
+    public abstract class BaseResolveDeleteClientErrorState<TModel>
+    {
+        public StateResult<TModel> DeleteLocally { get; } = new StateResult<TModel>();
+        public StateResult<TModel> MarkUnsyncable { get; } = new StateResult<TModel>();
+
+        public IObservable<ITransition> Start((ClientErrorException Reason, TModel Entity) parameter)
+            => IgnoreTheError(parameter.Reason)
+                ? Observable.Return(DeleteLocally.Transition(parameter.Entity))
+                : Observable.Return(MarkUnsyncable.Transition(parameter.Entity));
+
+        protected abstract bool IgnoreTheError(ClientErrorException error);
+    }
+}
---
+++ Toggl.Foundation/Sync/States/Push/ResolveDeleteTimeEntryClientErrorState.cs
@@ -0,0 +1,11 @@
+﻿using Toggl.PrimeRadiant.Models;
+using Toggl.Ultrawave.Exceptions;
+
+namespace Toggl.Foundation.Sync.States
+{
+    public sealed class ResolveDeleteTimeEntryClientErrorState : BaseResolveDeleteClientErrorState<IDatabaseTimeEntry>
+    {
+        protected override bool IgnoreTheError(ClientErrorException error)
+            => true;
+    }
+}

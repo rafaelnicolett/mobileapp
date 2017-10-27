a4316c328c9dab84f04be8d0c547d0eb64300569
simonrozsival <simon@rozsival.com>
2017-10-27 14:59:45 +0200

    :white_check_mark: Fix serialization test

--- Toggl.Ultrawave.Tests/Models/TimeEntryTests.cs
+++ Toggl.Ultrawave.Tests/Models/TimeEntryTests.cs
@@ -69,6 +69,9 @@ namespace Toggl.Ultrawave.Tests.Models
             private string validJson
                 => "{\"id\":525144694,\"workspace_id\":1414373,\"project_id\":3178352,\"task_id\":null,\"billable\":false,\"start\":\"2017-04-25T19:34:39+00:00\",\"stop\":null,\"duration\":-1493148879,\"description\":\"Some short description\",\"tag_ids\":[313040,3129041,319042],\"at\":\"2017-04-25T20:12:27+00:00\",\"server_deleted_at\":null,\"user_id\":0,\"created_with\":\"SomeApp\"}";
 
+            private string validJsonPosting
+                => "{\"id\":525144694,\"workspace_id\":1414373,\"project_id\":3178352,\"task_id\":null,\"billable\":false,\"start\":\"2017-04-25T19:34:39+00:00\",\"stop\":null,\"duration\":-1493148879,\"description\":\"Some short description\",\"tag_ids\":[313040,3129041,319042],\"at\":\"2017-04-25T20:12:27+00:00\",\"created_with\":\"SomeApp\"}";
+
             private IWorkspaceFeatureCollection pro
                 => new WorkspaceFeatureCollection { WorkspaceId = 1414373, Features = new IWorkspaceFeature[] { new WorkspaceFeature { FeatureId = WorkspaceFeatureId.Pro, Enabled = true } } };
 
@@ -113,7 +116,7 @@ namespace Toggl.Ultrawave.Tests.Models
             [Fact]
             public void CanBeSerializedForPosting()
             {
-                SerializationHelper.CanBeSerialized(validJson, validTimeEntry, SerializationReason.Post, pro);
+                SerializationHelper.CanBeSerialized(validJsonPosting, validTimeEntry, SerializationReason.Post, pro);
             }
         }
     }

9017e7d837c21a2cd3bbc64979a555408dbaff94
William Barbosa <william.sb@hotmail.com>
2017-11-01 15:25:11 -0200

    :construction: Add Firebase analytics service

---
+++ Toggl.Daneel/Services/FirebaseAnalyticsService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Firebase.Analytics;
+using Foundation;
+using Toggl.Foundation.MvvmCross.Services;
+
+namespace Toggl.Daneel.Services
+{
+    public sealed class FirebaseAnalyticsService : IAnalyticsService
+    {
+        private static readonly NSString[] pageTrackingKeys = { new NSString("ViewName") };
+
+        public void TrackClosing(Type type)
+        {
+            Analytics.LogEvent($"Page closed", getParameters(type));
+        }
+
+        public void TrackOpening(Type type)
+        {
+            Analytics.LogEvent($"Page opened", getParameters(type));
+        }
+
+        private static NSDictionary<NSString, NSObject> getParameters(Type type)
+        {
+            var values = new NSObject[] { new NSString(type.Name) };
+            var parameters = NSDictionary<NSString, NSObject>.FromObjectsAndKeys(
+                pageTrackingKeys, values, pageTrackingKeys.Length);
+
+            return parameters;
+        }
+    }
+}

b8542e0933dc50a302fa85bc7f0b86cc80969c36
Simon Rozsival <simon@rozsival.com>
2017-11-06 13:24:15 +0100

    :sparkles: Add states

---
+++ Toggl.Foundation/Sync/States/Push/Tags/DeleteLocalTagState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive;
+using Toggl.PrimeRadiant;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.Foundation.Sync.States
+{
+    public sealed class DeleteLocalTagState : BaseDeleteLocalEntityState<IDatabaseTag>
+    {
+        public DeleteLocalTagState(IRepository<IDatabaseTag> repository)
+            : base(repository)
+        {
+        }
+
+        protected override IObservable<Unit> Delete(IDatabaseTag entity)
+            => Repository.Delete(entity.Id);
+    }
+}
---
+++ Toggl.Foundation/Sync/States/Push/Tags/PushTagState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Toggl.Foundation.Models;
+using Toggl.PrimeRadiant;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.Foundation.Sync.States
+{
+    internal sealed class PushTagState : BasePushState<IDatabaseTag>
+    {
+        public PushTagState(ITogglDatabase database)
+            : base(database)
+        {
+        }
+
+        protected override IRepository<IDatabaseTag> GetRepository(IDatabaseTag database)
+            => database.Tags;
+
+        protected override DateTimeOffset LastChange(IDatabaseTag entity)
+            => entity.At;
+
+        protected override IDatabaseTag CopyFrom(IDatabaseTag entity)
+            => Tag.From(entity);
+    }
+}

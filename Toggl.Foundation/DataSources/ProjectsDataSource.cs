12d5bf2b1032c3786a7cd502881425730f29ba13
William Barbosa <william.sb@hotmail.com>
2017-11-06 20:08:12 -0200

    :construction: Add project creation to DataSource

--- Toggl.Foundation/DataSources/ProjectsDataSource.cs
+++ Toggl.Foundation/DataSources/ProjectsDataSource.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using Toggl.Foundation.DTOs;
+using Toggl.Foundation.Models;
 using Toggl.Multivac;
+using Toggl.Multivac.Extensions;
 using Toggl.PrimeRadiant;
 using Toggl.PrimeRadiant.Models;
 
@@ -8,15 +11,34 @@ namespace Toggl.Foundation.DataSources
 {
     public sealed class ProjectsDataSource : IProjectsSource
     {
+        private readonly IIdProvider idProvider;
+        private readonly ITimeService timeService;
         private readonly IRepository<IDatabaseProject> repository;
 
-        public ProjectsDataSource(IRepository<IDatabaseProject> repository)
+        public ProjectsDataSource(IIdProvider idProvider, IRepository<IDatabaseProject> repository, ITimeService timeService)
         {
+            Ensure.Argument.IsNotNull(idProvider, nameof(idProvider));
             Ensure.Argument.IsNotNull(repository, nameof(repository));
+            Ensure.Argument.IsNotNull(timeService, nameof(timeService));
 
             this.repository = repository;
+            this.idProvider = idProvider;
+            this.timeService = timeService;
         }
 
+        public IObservable<IDatabaseProject> Create(CreateProjectDTO dto)
+            => idProvider.GetNextIdentifier()
+                .Apply(Project.Builder.Create)
+                .SetName(dto.Name)
+                .SetColor(dto.Color)
+                .SetClientId(dto.ClientId)
+                .SetTemplateId(dto.TemplateId)
+                .SetWorkspaceId(dto.WorkspaceId)
+                .SetAt(timeService.CurrentDateTime)
+                .SetSyncStatus(SyncStatus.SyncNeeded)
+                .Build()
+                .Apply(repository.Create);
+        
         public IObservable<IEnumerable<IDatabaseProject>> GetAll()
             => repository.GetAll();
 

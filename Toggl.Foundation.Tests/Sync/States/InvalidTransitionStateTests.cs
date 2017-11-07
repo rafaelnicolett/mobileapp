25a3f6637b991ff1cd0bbc5053bb4167613e8e3a
simonrozsival <simon@rozsival.com>
2017-11-07 11:54:06 +0100

    :white_check_mark: Add tests

---
+++ Toggl.Foundation.Tests/Sync/States/InvalidTransitionStateTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FsCheck.Xunit;
+using Toggl.Foundation.Sync.States;
+using Xunit;
+using FluentAssertions;
+
+namespace Toggl.Foundation.Tests.Sync.States
+{
+    public sealed class InvalidTransitionStateTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("          ")]
+        public void ThrowsWhenTheConstructorArgumentIsNullOrEmpty(string message)
+        {
+            Action creatingState = () => new InvalidTransitionState(message);
+
+            creatingState.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ThrowsInvalidOperationException()
+        {
+            Exception caughtException = null;
+            var state = new InvalidTransitionState("Some message");
+
+            var observable = state.Start();
+            observable.Subscribe(_ => { }, (Exception exception) => caughtException = exception);
+
+            caughtException.Should().NotBeNull();
+            caughtException.Should().BeOfType<InvalidOperationException>();
+        }
+
+        [Property]
+        public void ThrowsInvalidOperationExceptionWithASpecificMessage(string message)
+        {
+            Exception caughtException = null;
+            var state = new InvalidTransitionState(message);
+
+            var observable = state.Start();
+            observable.Subscribe(_ => { }, (Exception exception) => caughtException = exception);
+
+            caughtException.Message.Should().Be($"Invalid transition: {message}");
+        }
+    }
+}
